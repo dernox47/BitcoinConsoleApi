764cab636b9fa3fe13be47cd5053bb45c106c5fc
Szin András <diak_393.szin.andras@12.d.2023.com>
2024-12-18 22:14:11 +0100

    build: create dart project

---
+++ bin/bitcoin_console_api.dart
@@ -0,0 +1,5 @@
+import 'package:bitcoin_console_api/bitcoin_console_api.dart' as bitcoin_console_api;
+
+void main(List<String> arguments) {
+  print('Hello world: ${bitcoin_console_api.calculate()}!');
+}
---
+++ test/bitcoin_console_api_test.dart
@@ -0,0 +1,8 @@
+import 'package:bitcoin_console_api/bitcoin_console_api.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
