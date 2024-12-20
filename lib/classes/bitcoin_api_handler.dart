3e0f13bedabc5b542466c3c93e3cb4bfcc5856fd
Szin András <diak_393.szin.andras@12.d.2023.com>
2024-12-20 12:38:20 +0100

    feat: create api handler

---
+++ lib/classes/bitcoin_api_handler.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+class BitcoinApiHandler {
+  static Uri url = Uri.parse('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest');
+
+  static Map<String, String> requestHeaders = {
+    'Content-type': 'application/json',
+    'X-CMC_PRO_API_KEY': 'ed27720a-6a74-4589-8e91-e4ba2bad5012'
+  };
+
+  static Future<Map<String, dynamic>> getCurrentData() async {
+    try {
+      final response = await http.get(url, headers: requestHeaders);
+      final data = jsonDecode(response.body);
+      return data;
+    } catch (e) {
+      throw e.toString();
+    }
+  }
+}
