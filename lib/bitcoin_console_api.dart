30d0e65dfb3e2acc399e52544734ab98d815dbcd
dernox47 <andras.szin47@gmail.com>
2024-12-21 11:34:24 +0100

    feat: create basic exchange actions in console

--- lib/bitcoin_console_api.dart
+++ lib/bitcoin_console_api.dart
@@ -1,23 +1,24 @@
 import 'dart:io';
+import 'dart:math';
 
 import 'package:bitcoin_console_api/classes/bitcoin_api_handler.dart';
+import 'package:bitcoin_console_api/classes/exchange.dart';
 import 'package:bitcoin_console_api/classes/file_handler.dart';
 import 'package:bitcoin_console_api/classes/user.dart';
 import 'package:intl/intl.dart';
 import 'package:collection/collection.dart';
-
 
 late List<User> users;
 late User selectedUser;
 
 void main() async {
   users = await FileHandler('users.json').getDataFromJson();
-  final currentData = await BitcoinApiHandler.getCurrentData();
-  final currentBitcoinData = currentData['data'][0];
+  var currentData = await BitcoinApiHandler.getCurrentData();
+  var currentBitcoinData = currentData['data'][0];
 
   bool appRunning = true;
   bool authTypeWaiting = true;
-  
+
   while (appRunning) {
     while (authTypeWaiting) {
       stdout.write('Login or register? (l/r): ');
@@ -53,13 +54,12 @@ void main() async {
           String passwordInput = inputCheck('\tPassword: ');
           var nextId = users.sortedBy((x) => x.id.toString()).last.id + 1;
           User testUser = User(
-            id: nextId,
-            name: nameInput,
-            dateOfBirth: dateOfBirthInput,
-            role: 'user',
-            username: usernameInput,
-            password: passwordInput
-          );
+              id: nextId,
+              name: nameInput,
+              dateOfBirth: dateOfBirthInput,
+              role: 'user',
+              username: usernameInput,
+              password: passwordInput);
 
           if (testUser.age() < 18) {
             print('The age requirement is 18.');
@@ -77,11 +77,46 @@ void main() async {
           print('Enter one of the options!');
       }
     }
-    print("\x1B[2J\x1B[0;0H");
+    Exchange exchange = Exchange(user: selectedUser);
+    var currentBitcoinPrice =
+        truncateToDecimalPlaces(currentBitcoinData['quote']['USD']['price'], 2);
+
+    clearConsole();
     print('Welcome ${selectedUser.name}!\n'
-          '(${selectedUser.age()} years old)');
-    print(DateTime.now());
+        '(${selectedUser.age()} years old)');
+
+    print(DateFormat('yyyy-MM-dd HH:mm:ss').format(DateTime.now()));
+
+    print('Balance:\n'
+          '${selectedUser.balance['USD']} USD\n'
+          '${selectedUser.balance['BTC']} BTC');
+    print('----------------------');
+    print('Current ${currentBitcoinData['name']} pirce\t$currentBitcoinPrice USD');
+    print('--------------');
+
+    print('Actions: buy(b), sell(s), refresh price(r), quit(q)');
+    var action = inputCheck('');
+    switch (action) {
+      case 'b':
+        var amount = inputCheck('Enter the amount of BTC: ');
+        exchange.buy(currentBitcoinPrice, double.parse(amount));
+        break;
+      case 's': 
+        var amount = inputCheck('Enter the amount of BTC: ');
+        exchange.sell(currentBitcoinPrice, double.parse(amount));
+        break;
+      case 'r':
+        currentData = await BitcoinApiHandler.getCurrentData();
+        currentBitcoinData = currentData['data'][0];
+        currentBitcoinPrice = truncateToDecimalPlaces(
+            currentBitcoinData['quote']['USD']['price'], 2);
+        print(currentBitcoinPrice);
+      default:
+        print('Enter one of the options!');
+    }
     
+    
+
     break;
   }
 }
@@ -117,3 +152,8 @@ User loginFrame() {
   }
   return findUser;
 }
+
+double truncateToDecimalPlaces(num value, int fractionalDigits) =>
+    (value * pow(10, fractionalDigits)).truncate() / pow(10, fractionalDigits);
+
+void clearConsole() => print("\x1B[2J\x1B[0;0H");
--- lib/classes/bitcoin_api_handler.dart
+++ lib/classes/bitcoin_api_handler.dart
@@ -4,7 +4,6 @@ import 'package:http/http.dart' as http;
 
 class BitcoinApiHandler {
   static Uri url = Uri.parse('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest');
-
   static Map<String, String> requestHeaders = {
     'Content-type': 'application/json',
     'X-CMC_PRO_API_KEY': 'ed27720a-6a74-4589-8e91-e4ba2bad5012'
